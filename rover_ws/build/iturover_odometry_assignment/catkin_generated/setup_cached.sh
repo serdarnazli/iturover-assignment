1d34e943c5440f312d5a5cc50ae5f23185403cbc
itu-itis21-nazlim21 <nserdarnazli@gmail.com>
2022-09-20 21:08:17 +0300

    rover_ws uploaded

---
+++ rover_ws/build/iturover_odometry_assignment/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/serdarnazli/rover_ws/devel/.private/iturover_odometry_assignment:$CMAKE_PREFIX_PATH"
+export PWD='/home/serdarnazli/rover_ws/build/iturover_odometry_assignment'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/serdarnazli/rover_ws/devel/.private/iturover_odometry_assignment/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/serdarnazli/rover_ws/src/iturover_odometry_assignment:$ROS_PACKAGE_PATH"
