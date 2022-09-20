76d2f54ec55e9c1fa4c88eeb615bd03968885afc
itu-itis21-nazlim21 <nserdarnazli@gmail.com>
2022-09-20 21:09:41 +0300

    rover_ws update

--- rover_ws/build/iturover_odometry_assignment/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/serdarnazli/rover_ws/src/iturover_odometry_assignment
-# Build directory: /home/serdarnazli/rover_ws/build/iturover_odometry_assignment
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
